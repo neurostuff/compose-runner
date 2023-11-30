1374b587d6d8244cb70f47eac7870bdc98d379d3
James Kent <jamesdkent21@gmail.com>
2023-11-30 13:14:38 -0600

    add no-upload argument

--- pynsc/cli.py
+++ pynsc/cli.py
@@ -15,10 +15,11 @@ from pynsc.run import run
 )
 @click.option("nsc_key", "--nsc-key", help="Neurosynth Compose api key.")
 @click.option("nv_key", "--nv-key", help="Neurovault api key.")
-def cli(meta_analysis_id, environment, result_dir, nsc_key, nv_key):
+@click.option("--no-upload", is_flag=True, help="Do not upload results.")
+def cli(meta_analysis_id, environment, result_dir, nsc_key, nv_key, no_upload):
     """Execute and upload a meta-analysis workflow.
 
     META_ANALYSIS_ID is the id of the meta-analysis on neurosynth-compose.
     """
-    url, _ = run(meta_analysis_id, environment, result_dir, nsc_key, nv_key)
+    url, _ = run(meta_analysis_id, environment, result_dir, nsc_key, nv_key, no_upload)
     print(url)
